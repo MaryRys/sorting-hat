edb58f38082fc427ee0916b7d3fbfe52613d7b5e
Mary Rys <rys.mary@gmail.com>
2018-09-26 19:56:46 -0500

    wip but cards are printing

--- main.js
+++ main.js
@@ -1,10 +1,37 @@
+let house = ['Gryffindor', 'Hufflepuff', 'Ravenclaw', 'Slytherin'];
+let randHouse = house[Math.floor(Math.random() * house.length)];
+
 const startForm = () => {
     document.getElementById('jumbotron').style.display = "none";
     document.getElementById('sortingForm').style.display = "block";
 }
 
+const printToDom = (stringToPrint, whereToPrint) => {
+    document.getElementById(whereToPrint).innerHTML += stringToPrint;
+}
+
 const sortingButton = document.getElementById('startSorting').addEventListener('click', startForm);
 
-// const startSortingGame = () => {
-//     let domString = 
-// }
+const buildNewStudentCard = (name, randHouse) => {
+    let domString = `<div class="card">
+    <div class="card-body">
+      <h5 class="card-title">${name}</h5>
+      <p class="card-text">${randHouse}</p>
+      <a href="#" class="btn btn-primary">Expel</a>
+    </div>
+  </div>`;
+
+    printToDom(domString, 'studentCards');
+}
+const submitSortingButton = document.getElementById('sortingButton');
+const nameInputElem = document.getElementById('nameInput');
+
+submitSortingButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    buildNewStudentCard(nameInputElem.value, randHouse)
+});
+
+// const isEmpty = () => {
+//     if (nameInputElem === "") {
+//         alert('Enter a name!');
+// }}
